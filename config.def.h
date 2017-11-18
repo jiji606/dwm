84198f60f65a4c8b11504192aa25a477b1b77a1e
Julia <p1ngu1n0fd00m@gmail.com>
2017-11-18 10:20:37 +0100

    Formatting

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 0;        /* gap between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int barheight = 2;       /* height of the bar */
+static const unsigned int barheight = 2;        /* height of the bar */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
@@ -16,11 +16,11 @@ static const char col_gray4[]  = "#eeeeee";
 static const char col_cyan[]   = "#005577";
 static const char *colors[][3] = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeSelTag] = { col_gray4, col_cyan, col_cyan },
-	[SchemeUrg]  = { col_gray1, col_cyan,  col_cyan  },
-	[SchemePerm] = { col_gray1, col_cyan,  col_gray4 },
+	[SchemeNorm]   = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]    = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSelTag] = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeUrg]    = { col_gray1, col_cyan,  col_cyan  },
+	[SchemePerm]   = { col_gray1, col_cyan,  col_gray4 },
 };
 
 /* tagging */
