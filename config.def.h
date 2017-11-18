2d68d028eb1e259ac2a6b4cb6bed35997d484304
Julia <p1ngu1n0fd00m@gmail.com>
2017-11-18 10:18:29 +0100

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -18,6 +18,7 @@ static const char *colors[][3] = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSelTag] = { col_gray4, col_cyan, col_cyan },
 	[SchemeUrg]  = { col_gray1, col_cyan,  col_cyan  },
 	[SchemePerm] = { col_gray1, col_cyan,  col_gray4 },
 };
